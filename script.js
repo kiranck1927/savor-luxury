05fa8fc148dee084f2760c02b30411feae082f97
kiranck1927 <kiranck1927@gmail.com>
2026-07-02 17:31:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Welcome message
+console.log("Welcome to Savor Luxury");
+
+// Select Discover More button
+const heroButton = document.querySelector(".hero-content button");
+
+// Button click event
+heroButton.addEventListener("click", function () {
+    alert("Welcome to Savor Luxury! Explore our delicious menu.");
+});
